e6602867ceaaf8d907472443927ab8bee7dd55c3
Marcel Koglin <rezssy@gmail.com>
2016-03-19 16:57:13 +0100

    Install gulp and gutil

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+(function () {
+    "use strict";
+    
+    /*global require */
+    var gulp = require('gulp'),
+        gutil = require('gulp-util');
+
+
+    gulp.task('log', function () {
+        gutil.log('Workflows are awesome');
+    });
+}());
+
